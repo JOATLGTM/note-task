2cce91bfe3cf6450d4d142f8b180c3c3d9a39031
Chris Vo <cv@Chriss-MBP.attlocal.net>
2024-08-07 17:23:17 -0500

    added types onto components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,21 @@ import "./App.css";
 import { Container } from "react-bootstrap";
 import NewNote from "./NewNote";
 
+export type Note = {
+	id: string;
+} & NoteData;
+
+export type NoteData = {
+	title: string;
+	markdown: string;
+	tags: Tag[];
+};
+
+export type Tag = {
+	id: string;
+	label: string;
+};
+
 function App() {
 	return (
 		<Container className="my-4">
--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -5,7 +5,7 @@ const NewNote = () => {
 	return (
 		<div>
 			<h1 className="mb-4">New Note</h1>
-			<NoteForm />
+			<NoteForm onSubmit={() => {}} />
 		</div>
 	);
 };
